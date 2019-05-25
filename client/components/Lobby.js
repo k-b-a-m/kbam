5daad4d0644913a3cce72eec1fde47d14769a98e
Bao Nguyen <bao.ng.nguyen3@gmail.com>
2019-05-25 09:58:44 -0400

    add buttons

--- client/components/Lobby.js
+++ client/components/Lobby.js
@@ -1,31 +1,28 @@
 import React from 'react';
 
 //fake seed
-const players = [
-  {id: 1, name: 'joe'},
-  {id: 2, name: 'john'},
-  {id: 3, name: 'wes'},
-  {id: 4, name: 'vil'},
-];
-const currentPlayer = {id: 1, name: 'joe'};
+const othersName = ['joe', 'john', 'vil', 'wes'];
+const currentPlayer = 'joe';
 
 //import style
 import '../styles/Lobby.css';
 const Lobby = () => {
   return (
     <div>
+      <div>
       <ol>
-        {players.map(player => (
+        {othersName.map((name, index) => (
           <li
-            key={player.id}
-            className={`${
-              player.name === currentPlayer.name ? 'current-player' : ''
-            }`}
+            key={index}
+            className={`${name === currentPlayer ? 'current-player' : ''}`}
           >
-            {player.name}
+            {name}
           </li>
         ))}
       </ol>
+      </div>
+      <button>Ready</button>
+      <button>Cancel</button>
     </div>
   );
 };
