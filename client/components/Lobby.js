a7e41410fffde4d438516ff8c7e860ee31fdbae4
Bao Nguyen <bao.ng.nguyen3@gmail.com>
2019-05-25 00:35:31 -0400

    add simple style to lobby

--- client/components/Lobby.js
+++ client/components/Lobby.js
@@ -1,24 +1,44 @@
-import React, {Component}from 'react';
+import React, {Component} from 'react';
 
 //import style
 import '../styles/Lobby.css';
 
-class Lobby extends Component{
-  constructor(){
+class Lobby extends Component {
+  constructor() {
     super();
-    this.state={
-      playerName:''
-    }
+    this.state = {
+      playerName: '',
+    };
   }
 
-  render(){
-    return(<div className='app-container'>
-    <form>
-      <input type="text" />
-      <button type="submit">Enter</button>
-    </form>
-  </div>)
+  handleChange = evt => {
+    this.setState({playerName: evt.target.value});
+  };
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+    console.log(this.state.playerName)
+  };
+
+  render() {
+    const {playerName} = this.state;
+    return (
+      <div className="lobby-container">
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input
+              className="name-input"
+              type="text"
+              placeholder="Enter Your Name"
+              value={playerName}
+              onChange={this.handleChange}
+            />
+          </div>
+          <button type="submit">Enter</button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Lobby
+export default Lobby;
