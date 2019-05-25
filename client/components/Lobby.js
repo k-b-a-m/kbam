8ea3329b91b63ce4074787d4484ad96b7fa67c91
Bao Nguyen <bao.ng.nguyen3@gmail.com>
2019-05-25 09:49:36 -0400

    bold ur own player

--- client/components/Lobby.js
+++ client/components/Lobby.js
@@ -1,17 +1,33 @@
 import React from 'react';
 
-
-
-
 //fake seed
-const players= ['joe','john','wes','vil']
+const players = [
+  {id: 1, name: 'joe'},
+  {id: 2, name: 'john'},
+  {id: 3, name: 'wes'},
+  {id: 4, name: 'vil'},
+];
+const currentPlayer = {id: 1, name: 'joe'};
 
+//import style
+import '../styles/Lobby.css';
 const Lobby = () => {
-  return <div>
-    <ul>
-      {players.map(player=><li>{player}</li>)}
-    </ul>
-  </div>;
+  return (
+    <div>
+      <ol>
+        {players.map(player => (
+          <li
+            key={player.id}
+            className={`${
+              player.name === currentPlayer.name ? 'current-player' : ''
+            }`}
+          >
+            {player.name}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 };
 
 export default Lobby;
